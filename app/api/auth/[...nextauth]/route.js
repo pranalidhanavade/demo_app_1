05c9139a72e2b641f30d55054b4b74cfaa304de0
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2025-03-17 19:41:56 +0530

    initial commit
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import KeycloakProvider from "next-auth/providers/keycloak";
+
+export const authOptions = {
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_CLIENT_ID,
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
+      issuer: process.env.KEYCLOAK_ISSUER,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
